3d347f8d779954d4135df1546cd50813f98f36dd
Joey <sjferenchak@gmail.com>
2020-10-23 14:07:46 -0400

    Fix blur, remove unnecessary variables

--- src/faceBlur.py
+++ src/faceBlur.py
@@ -1,36 +1,22 @@
 import numpy as np
 import cv2
 
-#set paths
 img_path = '/home/avrha/Desktop/faceBlur/pics/group1.jpg'
 xml_path = '/home/avrha/Desktop/faceBlur/pics/haarcascade_frontalface_default.xml'
 
-#read in image
 img = cv2.imread(img_path,1)
-
-#convert to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#create cascade object
 face_cascade =  cv2.CascadeClassifier(xml_path)
-
-#get width and height
-h, w = img.shape[:2]
-
-#detect faces 
 faces = face_cascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=5,minSize=(40,40),maxSize=(60,60))
 
+i = 1
 for (x,y,w,h) in faces:
-  cropped = img[y:y+h, x:w+w]
+  
+  cropped = img[y:y+h, x:x+w]
   cropped = cv2.GaussianBlur(cropped,(23,23),30)
   
-  cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-  print(cropped.shape[0])
-  l_img = img
-  s_img = cropped
-  y_offset = y
-  x_offset = x
-  l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
+  img[y:y+cropped.shape[0], x:x+cropped.shape[1]] = cropped 
 
 cv2.imshow("result",img)
 cv2.waitKey(0)
