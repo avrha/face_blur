bfe6e506bc5e0a347620e390ac5cd3910e3f3959
Joey <sjferenchak@gmail.com>
2020-10-23 14:27:40 -0400

    Add a folder for XML data, remove unused iterative variable

--- src/faceBlur.py
+++ src/faceBlur.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 img_path = '/home/avrha/Desktop/faceBlur/pics/group1.jpg'
-xml_path = '/home/avrha/Desktop/faceBlur/pics/haarcascade_frontalface_default.xml'
+xml_path = '/home/avrha/Desktop/faceBlur/xml/haarcascade_frontalface_default.xml'
 
 img = cv2.imread(img_path,1)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -10,7 +10,6 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 face_cascade =  cv2.CascadeClassifier(xml_path)
 faces = face_cascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=5,minSize=(40,40),maxSize=(60,60))
 
-i = 1
 for (x,y,w,h) in faces:
   
   cropped = img[y:y+h, x:x+w]
