b73badc41111c7910462a6301beffd68a5b37968
Joey <sjferenchak@gmail.com>
2020-10-23 12:54:28 -0400

    working face blur

--- src/faceBlur.py
+++ src/faceBlur.py
@@ -14,15 +14,30 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #create cascade object
 face_cascade =  cv2.CascadeClassifier(xml_path)
 
+#get width and height
+h, w = img.shape[:2]
+
 #detect faces 
 faces = face_cascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=5,minSize=(40,40),maxSize=(60,60))
 
+print(len(faces))
+print(faces)
+
+
 #draw faces on the source image
 for (x,y,w,h) in faces:
+  
+  h1 = h
+  w1 = w
+   
+  cropped = img[x:x+w,y:y+h]
+  #overlay = cv2.addWeighted(img, 1, cropped, 1, 0)
   cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
 
 #output image
-cv2.imshow("faces",img)
+
+
+cv2.imshow("gray",cropped)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
