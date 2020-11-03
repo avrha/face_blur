d2705ac81c64744a48d92f8d16dc3a4b0c9d5ea9
Joey <sjferenchak@gmail.com>
2020-11-03 00:25:33 -0500

    Implemented HOG, blurred faces

--- src/faceBlur.py
+++ src/faceBlur.py
@@ -1,22 +1,35 @@
-import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+import dlib
+import numpy as np
+from imutils import face_utils
+import matplotlib.image as mpimg
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 img_path = '/home/avrha/Desktop/faceBlur/pics/group1.jpg'
-xml_path = '/home/avrha/Desktop/faceBlur/xml/haarcascade_frontalface_default.xml'
 
-img = cv2.imread(img_path,1)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+source = mpimg.imread(img_path)
+blurred = source.copy()
 
-face_cascade =  cv2.CascadeClassifier(xml_path)
-faces = face_cascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=5,minSize=(40,40),maxSize=(60,60))
+gray =  cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+img = np.float32(gray) / 32
 
-for (x,y,w,h) in faces:
-  
-  cropped = img[y:y+h, x:x+w]
-  cropped = cv2.GaussianBlur(cropped,(23,23),30)
-  
-  img[y:y+cropped.shape[0], x:x+cropped.shape[1]] = cropped 
+#Calculate gradient
+gx = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=1)
+gy = cv2.Sobel(img, cv2.CV_32F, 0, 1, ksize=1)
+mag, angle = cv2.cartToPolar(gx,gy, angleInDegrees=True)
 
-cv2.imshow("result",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+face_detect = dlib.get_frontal_face_detector()
+
+rects = face_detect(gray,1)
+
+for (i,rect) in enumerate(rects):
+  (x, y, w, h) = face_utils.rect_to_bb(rect)
+  box = source[y:y+h, x:x+w]
+  box = cv2.GaussianBlur(box,(23,23),30)
+
+  blurred[y:y+box.shape[0], x:x+box.shape[1]] = box
+
+plt.figure(figsize=(12,8))
+plt.imshow(blurred)
+plt.show()
