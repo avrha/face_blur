92cb41d2025574a16d7fdf7799f038272c45d03d
Joey <sjferenchak@gmail.com>
2020-11-12 16:32:02 -0500

    Implemented CNN, very lagging though

--- src/faceBlurVid.py
+++ src/faceBlurVid.py
@@ -3,7 +3,6 @@ import dlib
 from imutils import face_utils
 
 cap = cv2.VideoCapture('../media/groupVid.mp4')
-face_detect = dlib.get_frontal_face_detector()
 
 if(cap.isOpened() == 0):
   print("Error opening the video file")
@@ -11,16 +10,22 @@ if(cap.isOpened() == 0):
 while(cap.isOpened()):
   ret, frame = cap.read()
   if ret == 1:
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    rects = face_detect(gray,1)
+    cnnFaceDetector = dlib.cnn_face_detection_model_v1('../model/mmod_human_face_detector.dat')
+    faces = cnnFaceDetector(gray,1)
 
-    for (i,rects) in enumerate(rects):
-      (x,y,w,h) = face_utils.rect_to_bb(rects)
+    for faceRect in faces:
+      rect = faceRect.rect
+      x = rect.left()
+      y = rect.top()
+      w = rect.right() - x
+      h = rect.bottom() - y
+
       box = frame[y:y+h,x:x+w]
-      box = cv2.GaussianBlur(box,(23,23,),30)
-      frame[y:y+box.shape[0],x:x+box.shape[1]] = box
+      box = cv2.GaussianBlur(box,(23,23),30)
+      frame[y:y+box.shape[0], x:x+box.shape[1]] = box 
 
       cv2.imshow('Video',frame)
+      print(box)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
       break
