d41f72557de2a6acc5379f52fa56e81e32a7838e
Joey <sjferenchak@gmail.com>
2020-11-17 23:29:47 -0500

    add argument support

--- src/cnn/cnnPic.py
+++ src/cnn/cnnPic.py
@@ -2,11 +2,18 @@ import sys
 import cv2
 from cnnBlur import face_blur
 import dlib
+import argparse
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True, help="path to input file")
+ap.add_argument("-s", "--skip", type=int,default=1,help="skip value")
+args = vars(ap.parse_args())
 
 
 def main():
     # Load in and convert source image to into grayscale
-    img = cv2.imread('../../media/groupPic.jpg', 1)
+    img = cv2.imread(args["input"], 1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.resize(gray, (0, 0), fx=0.25, fy=0.25)
     frame_count = 0
--- src/cnn/cnnVid.py
+++ src/cnn/cnnVid.py
@@ -4,11 +4,18 @@
 
 import cv2
 import dlib
+import argparse
 from imutils import face_utils
-from cnnBlur import faceBlur
+from cnnBlur import face_blur 
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True, help="path to input file")
+ap.add_argument("-s", "--skip", type=int,default=1,help="skip value")
+args = vars(ap.parse_args())
+
 
 def main():
-    cap = cv2.VideoCapture('../../media/groupVid.mp4')
+    cap = cv2.VideoCapture(args["input"])
     cnnFaceDetector = dlib.cnn_face_detection_model_v1('../../model/mmod_human_face_detector.dat')
     
     if cap.isOpened() == 0:
@@ -21,12 +28,11 @@ def main():
     
         if ret == 1:
             # skip frame
-            if cap.get(1) % 5 == 0:
-                faces = cnnFaceDetector(gray, 0)
-                frameCount = cap.get(1)
-                faceCount = 0
-    
-                faceBlur(frame, faces, frameCount, faceCount)
+            if cap.get(1) % args["skip"] == 0:
+                faces = cnnFaceDetector(gray, 1)
+                print("Frame:",cap.get(1)) 
+
+                face_blur(frame, faces)
     
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
