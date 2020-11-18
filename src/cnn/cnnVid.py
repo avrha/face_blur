f0a630205c93f914a6c4da769233dcb8701f0b34
Joey <sjferenchak@gmail.com>
2020-11-17 21:25:16 -0500

    Add main function

--- src/cnn/cnnVid.py
+++ src/cnn/cnnVid.py
@@ -7,31 +7,35 @@ import dlib
 from imutils import face_utils
 from cnnBlur import faceBlur
 
-cap = cv2.VideoCapture('../../media/groupVid.mp4')
-cnnFaceDetector = dlib.cnn_face_detection_model_v1('../../model/mmod_human_face_detector.dat')
-
-if cap.isOpened() == 0:
-    print("Error opening the video file")
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.resize(gray, (0, 0), fx=0.25, fy=0.25)
-
-    if ret == 1:
-        # skip frame
-        if cap.get(1) % 5 == 0:
-            faces = cnnFaceDetector(gray, 0)
-            frameCount = cap.get(1)
-            faceCount = 0
-
-            faceBlur(frame, faces, frameCount, faceCount)
-
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+def main():
+    cap = cv2.VideoCapture('../../media/groupVid.mp4')
+    cnnFaceDetector = dlib.cnn_face_detection_model_v1('../../model/mmod_human_face_detector.dat')
+    
+    if cap.isOpened() == 0:
+        print("Error opening the video file")
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.resize(gray, (0, 0), fx=0.25, fy=0.25)
+    
+        if ret == 1:
+            # skip frame
+            if cap.get(1) % 5 == 0:
+                faces = cnnFaceDetector(gray, 0)
+                frameCount = cap.get(1)
+                faceCount = 0
+    
+                faceBlur(frame, faces, frameCount, faceCount)
+    
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+    
+        else:
             break
+    
+    cap.release()
+    cv2.destroyAllWindows()
 
-    else:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()
