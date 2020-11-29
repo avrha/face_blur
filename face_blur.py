76544f89fb4590f0746d368d102e8c51e6960bdd
Joey <sjferenchak@gmail.com>
2020-11-29 00:43:24 -0500

    add an argument for skip frame

--- face_blur.py
+++ face_blur.py
@@ -8,6 +8,7 @@ from src.face_detection import image, video
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True, help="Path to image file")
 ap.add_argument("-o", "--output", required=True, help="Name of output file")
+ap.add_argument("-s", "--skip frame", required=False, type=int,default=1, help="Value to skip frame")
 args = vars(ap.parse_args())
 
 
@@ -17,7 +18,8 @@ def main():
 
   else:
     print("this is a video file")
-    video(args["input"],args["output"]) 
+    video(args["input"],args["output"],args["skip frame"]) 
+
 
 if __name__ == "__main__":
   main()
