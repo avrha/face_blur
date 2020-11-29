5cb92a49d40ff10e811b8beb50ab88fc40c9dbec
Joey <sjferenchak@gmail.com>
2020-11-28 23:46:50 -0500

    proper formatting

--- face_blur.py
+++ face_blur.py
@@ -3,6 +3,7 @@ import argparse
 import cv2
 from src.face_detection import image, video
 
+
 # add arguments 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True, help="Path to image file")
@@ -13,8 +14,10 @@ args = vars(ap.parse_args())
 def main():
   if cv2.haveImageReader(args["input"]) == True:
     image(args["input"],args["output"])
+
   else:
     print("this is a video file")
     video(args["input"],args["output"]) 
+
 if __name__ == "__main__":
   main()
