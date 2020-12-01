ae8399165b5bbbdeaf9efd6f9019f1f60bb3901a
Joey <sjferenchak@gmail.com>
2020-11-30 22:44:43 -0500

    add a resize agrument

--- face_blur.py
+++ face_blur.py
@@ -9,12 +9,13 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True, help="Path to image file")
 ap.add_argument("-o", "--output", required=True, help="Name of output file")
 ap.add_argument("-s", "--skip frame", required=False, type=int,default=1, help="Value to skip frame")
+ap.add_argument("-r","--resize image", required=False, type=float,default=1, help="Value to resize frame")
 args = vars(ap.parse_args())
 
 
 def main():
   if cv2.haveImageReader(args["input"]) == True:
-    image(args["input"],args["output"])
+    image(args["input"],args["output"],args["resize image"])
 
   else:
     print("this is a video file")
